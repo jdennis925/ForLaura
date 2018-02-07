aa36fa8f3bdb44e12052937b213d36dfdd9ea64e
John Dennis <jdennis925@gmail.com>
2018-02-06 20:40:28 -0600

    cleanup

--- 100 list/code.py
+++ 100 list/code.py
@@ -1,3 +1,5 @@
+max = 10
+
 def FactMult(theNumber, theMax):
     result = []
     for i in range(1, theNumber):
@@ -11,44 +13,42 @@ def FactMult(theNumber, theMax):
     result.remove(theNumber)
     return result
 
-max = 100
 result_new = {}
 for k in range(1, max + 1):
     result_new[k] = FactMult(k,max)
 
+#uncomment to create txt file of all factors
+#f = open('factsAndMults.txt', 'w')
+#f.write((str)(result_new))
+#for i in result_new:
+    #f.write(str(i))
+    #f.write(('{}:{}\n'.format(i, result_new[i])))
+
 print(result_new)
 
-f = open('laura.csv', 'w')
-
-for i in result_new:
-    for j in result_new[i]:
-        f.write(str(i)+ ',')
-        f.write(str(j) + ',\n')
-
-# def FindFunc(curList):
-#     lastValue = curList[len(curList)-1]
-#     multiplesAndFactors = result_new[lastValue]
-#     resultList = []
-#     for item in multiplesAndFactors:
-#         if item not in curList:
-#             newList = list(curList)
-#             newList.append(item)
-#             resultList.append(newList)
-#             otherList = FindFunc(newList)
-#             for n in otherList:
-#                 resultList.append(n)
-#     return resultList
+def FindFunc(curList):
+    lastValue = curList[len(curList)-1]
+    multiplesAndFactors = result_new[lastValue]
+    resultList = []
+    for item in multiplesAndFactors:
+        if item not in curList:
+            newList = list(curList)
+            newList.append(item)
+            resultList.append(newList)
+            otherList = FindFunc(newList)
+            for n in otherList:
+                resultList.append(n)
+    return resultList
 
 
-# longestList = []
-# for m in range(2, max + 1):
-#     x = FindFunc([m])
-#     for tempList in x:
-#         if (len(tempList) > len(longestList)):
-#             longestList = tempList
-#             print(longestList)
+longestList = []
+for m in range(2, max + 1):
+    x = FindFunc([m])
+    for tempList in x:
+        if (len(tempList) > len(longestList)):
+            longestList = tempList
+            print(longestList)
 
-# print('List Length')
-# print(len(longestList))
-# print(longestList)
+print('List Length: {}'.format(len(longestList)))
+print(longestList)
 
